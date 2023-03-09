258b6627c2f39aa7c19c03cdddbd40dbd8bf1c40
benis <benis@LAPTOP-MBO1BQDA>
2023-03-09 19:12:15 +0300

    Добавьте файлы проекта.

---
+++ WSUniversalLib/Calculation.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace WSUniversalLib
+{
+    public class Calculation
+    {
+        public static int GetQuantityForProduct(int product_type, int material_type, int count, float width, float length)
+        {
+            const float material_type1 = 0.003f;
+            const float material_type2 = 0.0012f;
+            const float product_type1 = 1.1f;
+            const float product_type2 = 2.5f;
+            const float product_type3 = 8.43f;
+
+            float a = count * width * length;
+
+            switch (product_type)
+            {
+                default:
+                    return -1;
+
+                case 1:
+                    a *= product_type1;
+                    break;
+
+                case 2:
+                    a *= product_type2;
+                    break;
+
+                case 3:
+                    a *= product_type3;
+                    break;
+            }
+
+            switch (material_type)
+            {
+                default:
+                    return -1;
+                case 1:
+                    a += material_type1 * a;
+                    break;
+
+                case 2:
+                    a += material_type2 * a;
+                    break;
+            }
+
+            return Convert.ToInt32(Math.Round((decimal)a, 0, MidpointRounding.AwayFromZero));
+        }
+    }
+}
---
+++ WSUniversalLibTests/CalculationTests.cs
@@ -0,0 +1,169 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WSUniversalLib;
+
+namespace WSUniversalLib.Tests
+{
+    [TestClass()]
+    public class CalculationTests
+    {
+        [TestMethod()]
+        public void GetQuantityForProductTest_CorrectData()
+        {
+            //arrange
+            int productType = 3;
+            int materialType = 1;
+            int count = 15;
+            int width = 20;
+            int lenght = 45;
+            int expected = 114146;
+            //act
+            int actual = Calculation.GetQuantityForProduct(productType, materialType, count, width, lenght);
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void GetQuantityForProductTest_IncorectProductType()
+        {
+            //arrange
+            int productType = 50;
+            int materialType = 1;
+            int count = 15;
+            int width = 20;
+            int lenght = 45;
+            int expected = -1;
+            //act
+            int actual = Calculation.GetQuantityForProduct(productType, materialType, count, width, lenght);
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void GetQuantityForProductTest_IncorectMaterialType()
+        {
+            //arrange
+            int productType = 3;
+            int materialType = 11;
+            int count = 15;
+            int width = 20;
+            int lenght = 45;
+            int expected = -1;
+            //act
+            int actual = Calculation.GetQuantityForProduct(productType, materialType, count, width, lenght);
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void GetQuantityForProductTest_IncorectProductType_MaterialType()
+        {
+            //arrange
+            int productType = 50;
+            int materialType = 90;
+            int count = 15;
+            int width = 20;
+            int lenght = 45;
+            int expected = -1;
+            //act
+            int actual = Calculation.GetQuantityForProduct(productType, materialType, count, width, lenght);
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void GetQuantityForProductTest_IncorectCount()
+        {
+            //arrange
+            int productType = 3;
+            int materialType = 1;
+            int count = 5;
+            int width = 20;
+            int lenght = 45;
+            int expected = 38049;
+            //act
+            int actual = Calculation.GetQuantityForProduct(productType, materialType, count, width, lenght);
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void GetQuantityForProductTest_IncorectCount_Lenght()
+        {
+            //arrange
+            int productType = 3;
+            int materialType = 1;
+            int count = 5;
+            int width = 20;
+            int lenght = 14;
+            int expected = 11837;
+            //act
+            int actual = Calculation.GetQuantityForProduct(productType, materialType, count, width, lenght);
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void GetQuantityForProductTest_IncorectCount_Widht()
+        {
+            //arrange
+            int productType = 3;
+            int materialType = 1;
+            int count = 6;
+            int width = 7;
+            int lenght = 45;
+            int expected = 15981;
+            //act
+            int actual = Calculation.GetQuantityForProduct(productType, materialType, count, width, lenght);
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void GetQuantityForProductTest_IncorectLenght_Widht()
+        {
+            //arrange
+            int productType = 3;
+            int materialType = 1;
+            int count = 15;
+            int width = 2;
+            int lenght = 13;
+            int expected = 3298;
+            //act
+            int actual = Calculation.GetQuantityForProduct(productType, materialType, count, width, lenght);
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void GetQuantityForProductTest_IncorectCount_Lenght_Widht()
+        {
+            //arrange
+            int productType = 3;
+            int materialType = 1;
+            int count = 3;
+            int width = 7;
+            int lenght = 17;
+            int expected = 3019;
+            //act
+            int actual = Calculation.GetQuantityForProduct(productType, materialType, count, width, lenght);
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void GetQuantityForProductTest_IncorectCount_MaterialType_ProductType()
+        {
+            //arrange
+            int productType = -3;
+            int materialType = 5;
+            int count = 0;
+            int width = 20;
+            int lenght = 45;
+            int expected = -1;
+            //act
+            int actual = Calculation.GetQuantityForProduct(productType, materialType, count, width, lenght);
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
